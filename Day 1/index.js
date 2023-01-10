3c9f76101a2d5475ca466372638be7a73290d482
Vedant Giri <113013212+vedant-giri@users.noreply.github.com>
2023-01-10 18:37:13 +0530

    Add files via upload

---
+++ Day 1/index.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+let firstName = 'Vedant'    // first name of a person
+let lastName = 'Giri'      // last name of a person
+let country = 'India'       // country
+let city = 'Wardha'         // capital city
+let age = 100                 // age in years
+let isMarried = true
+
+// Declaring variables with number values
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'Vedant',     //name of a person
+  job = 'Student',
+  live = 'India'
+
+console.log(firstName, lastName, country, city, age, isMarried) 
+console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
+console.log(name, job, live)
