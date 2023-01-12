1d84821170ac4030cfbae6c30248ac4f90bf3409
Vedant Giri <113013212+vedant-giri@users.noreply.github.com>
2023-01-12 16:01:51 +0530

    Add files via upload

---
+++ Day 3/index.js
@@ -0,0 +1,38 @@
+// Arithmetic operators
+let a = 5;
+let b = 2;
+console.log(a + b); // 7
+console.log(a - b); // 3
+console.log(a * b); // 10
+console.log(a / b); // 2.5
+console.log(a % b); // 1
+
+// Comparison operators
+console.log(a == b); // false
+console.log(a === b); // false
+console.log(a != b); // true
+console.log(a !== b); // true
+console.log(a > b); // true
+console.log(a < b); // false
+console.log(a >= b); // true
+console.log(a <= b); // false
+
+// Logical operators
+let c = true;
+let d = false;
+console.log(c && d); // false
+console.log(c || d); // true
+console.log(!c); // false
+
+// Assignment operators
+let e = 10;
+e += 5; // e is now 15
+e -= 3; // e is now 12
+e *= 2; // e is now 24
+e /= 4; // e is now 6
+e %= 3; // e is now 0
+
+// Conditional (ternary) operator
+let age = 25;
+let canDrinkAlcohol = (age >= 21) ? "can drink" : "cannot drink";
+console.log(canDrinkAlcohol); // "can drink"
