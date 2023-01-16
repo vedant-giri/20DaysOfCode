8fd76d94c23a509011e61ef5448d7b8a493d0939
Vedant Giri <113013212+vedant-giri@users.noreply.github.com>
2023-01-16 16:50:01 +0530

    Add files via upload

---
+++ Day 7/functions.js
@@ -0,0 +1,65 @@
+//declaring a function without a parameter
+function functionName() {
+    // code goes here
+  }
+  functionName() // calling function by its name and with parentheses
+
+  // function without parameter,  a function which make a number square
+function square() {
+    let num = 2
+    let sq = num * num
+    console.log(sq)
+  }
+  
+  square() // 4
+  
+  // function without parameter
+  function addTwoNumbers() {
+    let numOne = 10
+    let numTwo = 20
+    let sum = numOne + numTwo
+  
+    console.log(sum)
+  }
+  
+  addTwoNumbers() // a function has to be called by its name to be executed 
+
+  function printFullName (){
+    let firstName = 'Asabeneh'
+    let lastName = 'Yetayeh'
+    let space = ' '
+    let fullName = firstName + space + lastName
+    console.log(fullName)
+}
+
+printFullName() // calling a function
+
+// function with one parameter
+function functionName(parm1) {
+    //code goes her
+  }
+  functionName(parm1) // during calling or invoking one argument needed
+  
+  function areaOfCircle(r) {
+    let area = Math.PI * r * r
+    return area
+  }
+  
+  console.log(areaOfCircle(10)) // should be called with one argument
+  
+  function square(number) {
+    return number * number
+  }
+  
+  console.log(square(10))
+
+  // Let us access the arguments object
+​
+const sumAllNums = (...args) => {
+ // console.log(arguments), arguments object not found in arrow function
+ // instead we use a parameter followed by spread operator (...)
+ console.log(args)
+}
+
+sumAllNums(1, 2, 3, 4)
+// [1, 2, 3, 4]
