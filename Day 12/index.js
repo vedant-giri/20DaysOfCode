bff2839e0f461f82c1900518fafdc77e5bd88eae
Vedant Giri <113013212+vedant-giri@users.noreply.github.com>
2023-01-21 13:38:59 +0530

    Add files via upload

---
+++ Day 12/index.js
@@ -0,0 +1,25 @@
+var allTitles = document.getElementsByClassName('title')
+
+console.log(allTitles) //HTMLCollections
+console.log(allTitles.length) // 4
+
+for (var i = 0; i < allTitles.length; i++) {
+  console.log(allTitles[i]) // prints each elements in the HTMLCollection
+}
+
+var allTitles = document.getElementsByClassName('title')
+
+console.log(allTitles) //HTMLCollections
+console.log(allTitles.length) // 4
+
+for (var i = 0; i < allTitles.length; i++) {
+  console.log(allTitles[i]) // prints each elements in the HTMLCollection
+}
+
+var firstTitle = document.getElementById('first-title')
+console.log(firstTitle) // <h1>First Title</h1>
+
+
+var firstTitle = document.querySelector('h1') // select the first available h1 element
+var firstTitle = document.querySelector('#first-title') // select id with first-title
+var firstTitle = document.querySelector('.title') // select the first available element with class title
