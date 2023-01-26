48b5fbc4f4475b487ca551663a548b8c15e192b8
Vedant Giri <113013212+vedant-giri@users.noreply.github.com>
2023-01-26 18:46:37 +0530

    Add files via upload

---
+++ Day 17/promises.js
@@ -0,0 +1,40 @@
+//Callback
+const doSomething = callback => {
+    setTimeout(() => {
+      const skills = ['HTML', 'CSS', 'JS']
+      callback('It did not go well', skills)
+    }, 2000)
+  }
+  
+  const callback = (err, result) => {
+    if (err) {
+      return console.log(err)
+    }
+    return console.log(result)
+  }
+  
+  doSomething(callback)
+
+  // syntax
+const promise = new Promise((resolve, reject) => {
+    resolve('success')
+    reject('failure')
+  })
+
+  // Promise
+const doPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const skills = ['HTML', 'CSS', 'JS']
+      if (skills.length > 0) {
+        resolve(skills)
+      } else {
+        reject('Something wrong has happened')
+      }
+    }, 2000)
+  })
+  
+  doPromise
+    .then(result => {
+      console.log(result)
+    })
+    .catch(error => console.log(error))
